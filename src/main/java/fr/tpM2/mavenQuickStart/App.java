82bec3a8f19450bdafa7948bde51794707193ad7
prune <prunefd@gmail.com>
2018-09-12 10:59:02 +0200

    new

---
+++ src/main/java/fr/tpM2/mavenQuickStart/App.java
@@ -0,0 +1,13 @@
+package fr.tpM2.mavenQuickStart;
+
+/**
+ * TP1 MAN Maven : goo.gl/oiTgUs
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
